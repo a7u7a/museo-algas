22b136796c0bb9a2637933ee6265a9b914b44834
a7u7a <esteban@esrs.co>
2022-02-07 17:55:34 -0300

    wip _path

--- pages/catalogo/_path.vue
+++ pages/catalogo/_path.vue
@@ -1,35 +1,46 @@
 <template>
   <div>
-    <div class="fixed pt-24 w-72 pl-24 font-serif">
-        <div class="text-3xl italic text-right"> {{alga.ficha.especie}}</div>
-        <div class="flex flex-col mt-2 space-y-2 text-right">
-            <p>Características</p>
-            <p>Representación</p>
-            <p>Herborización</p>
-        </div>
-    </div>
-    <div class="flex flex-col w-screen items-center font-serif">
-      <div class="pt-24 w-7/12">
-        <div>
-          <div class="text-3xl italic">{{ alga.ficha.especie }}</div>
-          <div class="flex mt-4 flex-row space-x-4">
+    <div class="flex flex-row">
+      <!-- side title -->
+      <div>
+        <div
+          :class="{ 'opacity-100': showSideHeader }"
+          class="fixed pt-24 w-72 pl-24 font-serif opacity-0"
+        >
+          <div class="text-3xl italic text-right">{{ alga.ficha.especie }}</div>
+          <div class="flex flex-col mt-2 space-y-2 text-right">
             <p>Características</p>
             <p>Representación</p>
             <p>Herborización</p>
           </div>
         </div>
+      </div>
+      
+      <!-- main content -->
+      <div>
+        <div class="flex flex-col w-screen items-center font-serif">
+          <div class="pt-24 w-7/12">
+            <div>
+              <div class="text-3xl italic">{{ alga.ficha.especie }}</div>
+              <div class="flex mt-4 flex-row space-x-4">
+                <p>Características</p>
+                <p>Representación</p>
+                <p>Herborización</p>
+              </div>
+            </div>
 
-        <div class="text-xl mt-8">Características</div>
-        <classTable :tabla="alga.ficha.tabla" />
+            <div class="text-xl mt-8">Características</div>
+            <classTable :tabla="alga.ficha.tabla" />
 
-        <div class="mt-8">
-          <div class="mb-4" v-for="(info, i) in alga.ficha.info" :key="i">
-            <a class="font-bold">{{ info.nombre }}:</a>
-            <a>{{ info.data }}</a>
+            <div class="mt-8">
+              <div class="mb-4" v-for="(info, i) in alga.ficha.info" :key="i">
+                <a class="font-bold">{{ info.nombre }}:</a>
+                <a>{{ info.data }}</a>
+              </div>
+            </div>
+            <webGLViewport />
           </div>
         </div>
-
-        <webGLViewport />
       </div>
     </div>
   </div>
@@ -37,16 +48,26 @@
 
 <script>
 export default {
-  mounted: function () {
-    window.addEventListener("scroll", function () {
-      console.log(window.scrollY);
-    });
+  data: function () {
+    return {
+      showSideHeader: false,
+      headerThres: 115,
+    };
   },
-  // methods:{
-  //     test: function(){
-  //         console.log(window.scrollY)
-  //     }
-  // },
+  mounted: function () {
+    window.addEventListener("scroll", this.test);
+  },
+  methods: {
+    test: function () {
+      console.log(window.scrollY, this.showSideHeader);
+      console.log("this.headerThres", this.headerThres);
+      if (window.scrollY > this.headerThres) {
+        this.showSideHeader = true;
+      } else {
+        this.showSideHeader = false;
+      }
+    },
+  },
   computed: {
     alga() {
       return this.$store.getters.getAlgaByPath(this.$route.params.path);
---
+++ pages/catalogo/_path2.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="flex flex-row">
+      <!-- side title -->
+      <div
+        :class="{ 'opacity-100': showSideHeader }"
+        class="fixed pt-24 w-72 pl-24 font-serif opacity-0"
+      >
+        <div class="text-3xl italic text-right">{{ alga.ficha.especie }}</div>
+        <div class="flex flex-col mt-2 space-y-2 text-right">
+          <p>Características</p>
+          <p>Representación</p>
+          <p>Herborización</p>
+        </div>
+      </div>
+
+      <!-- main content -->
+      <div class="flex w-7/12 flex-col w-screen items-center font-serif">
+        <div class="pt-24 ">
+          <div>
+            <div class="text-3xl italic">{{ alga.ficha.especie }}</div>
+            <div class="flex mt-4 flex-row space-x-4">
+              <p>Características</p>
+              <p>Representación</p>
+              <p>Herborización</p>
+            </div>
+          </div>
+
+          <div class="text-xl mt-8">Características</div>
+          <classTable :tabla="alga.ficha.tabla" />
+
+          <div class="mt-8">
+            <div class="mb-4" v-for="(info, i) in alga.ficha.info" :key="i">
+              <a class="font-bold">{{ info.nombre }}:</a>
+              <a>{{ info.data }}</a>
+            </div>
+          </div>
+          <webGLViewport />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      showSideHeader: false,
+      headerThres: 115,
+    };
+  },
+  mounted: function () {
+    window.addEventListener("scroll", this.test);
+  },
+  methods: {
+    test: function () {
+      console.log(window.scrollY, this.showSideHeader);
+      console.log("this.headerThres", this.headerThres);
+      if (window.scrollY > this.headerThres) {
+        this.showSideHeader = true;
+      } else {
+        this.showSideHeader = false;
+      }
+    },
+  },
+  computed: {
+    alga() {
+      return this.$store.getters.getAlgaByPath(this.$route.params.path);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
