63d256fad4d28c83533067ae145fc2c8a9b56c28
a7u7a <esteban@esrs.co>
2023-01-05 12:17:45 -0300

    typo

--- components/about.vue
+++ components/about.vue
@@ -7,11 +7,11 @@
       <div class="pl-8 md:pl-0 pr-8">
         <p class="font-sans text-center">
           Este proyecto nace de la necesidad de educar y concientizar sobre el
-          valor ecológico y cultural de las algas las regiones de;  Los
-          Lagos, Los Ríos, La Araucania, Maule, O`Higgins, Valparaiso
-          y Coquimbo. Este museo digital, Museo de las Algas, se concibe como un
+          valor ecológico y cultural de las algas las regiones de Los
+          Lagos, Los Ríos, La Araucania, Maule, O`Higgins, Valparaiso
+          y Coquimbo. Este museo digital, Museo de las Algas, se concibe como un
           espacio de aprendizaje, conservación y contemplación de una selección
-          de Algas endémicas de las costas de Chile. Proporciona herramientas
+          de Algas endémicas de las costas de Chile. Proporciona herramientas
           para que podamos desarrollar un entendimiento más integral del mundo
           de la filología, desde un punto de vista científico, social, y
           cultural.
@@ -19,7 +19,7 @@
           <br>
           Este proyecto es posible gracias al financiamiento de la
           Fundación Mar Adentro mediante su Fondo FMA, y gracias a un equipo que
-          soñó y se fascinó con el mundo de las algas.Éste pretende ser solo el
+          soñó y se fascinó con el mundo de las algas. Éste pretende ser solo el
           comienzo de un sinfín de formas de interacción y coexistencia con el
           mundo fascinante que nos entrega las Algas.
           <br>
